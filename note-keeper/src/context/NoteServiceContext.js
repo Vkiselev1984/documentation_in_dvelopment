57562ac1ea4177398e0b2857401425b3e1e9f7c9
Vladimir Kiselev <vkiselev-opec@mail.ru>
2025-07-30 02:30:34 +0300

    Delete note-keeper directory

--- note-keeper/src/context/NoteServiceContext.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import NoteService from '../services/NoteService';
-export const NoteServiceContext = React.createContext(NoteService);
--- note-keeper/src/interfaces/INoteService.js
+++
@@ -1,7 +0,0 @@
-
-export default class INoteService {
-    addNote(note) { throw new Error('Not implemented'); }
-    editNote(note) { throw new Error('Not implemented'); }
-    deleteNote(id) { throw new Error('Not implemented'); }
-    getNotes() { throw new Error('Not implemented'); }
-}
